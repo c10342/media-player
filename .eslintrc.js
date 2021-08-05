c3241e4f0d72d28671e34d5a33638d8cb055df58
c10342 <1376023740@qq.com>
2021-08-05 23:12:42 +0800

    feat(eslint): 调整eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,14 @@ module.exports = {
     es2021: true
   },
   parser: "@typescript-eslint/parser", //定义ESLint的解析器
-  extends: ["plugin:@typescript-eslint/recommended",'plugin:prettier/recommended'], //定义文件继承的子规范
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ], //定义文件继承的子规范
   plugins: ["@typescript-eslint"], //定义了该eslint文件所依赖的插件
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off"
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-var-requires": "off"
   }
 };
