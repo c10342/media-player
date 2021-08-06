e31be09d1ce18a5adbe9fae9646a1f9e492c4c32
c10342 <1376023740@qq.com>
2021-08-06 23:29:03 +0800

    examples(examples): 测试demo

--- examples/index.ts
+++ examples/index.ts
@@ -1,5 +1,14 @@
 import VideoPlayer from "@media/player";
+import "./index.scss";
+
+// console.log(a);
 
 const player = new VideoPlayer({
-  el: ".container"
+  el: ".container",
+  videoList: [
+    {
+      label: "高清",
+      url: "https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4"
+    }
+  ]
 });
