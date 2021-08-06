24dc38fa76054f28d91b3aebd58018b5b403cb14
c10342 <1376023740@qq.com>
2021-08-06 21:07:57 +0800

    chore(.eslintrc.js): 添加eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-var-requires": "off"
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/ban-types": "off"
   }
 };
