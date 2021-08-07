0c8b8079982054608863971170f1b8609693a060
c10342 <1376023740@qq.com>
2021-08-07 19:50:00 +0800

    examples(examples): 测试demo

--- examples/index.ts
+++ examples/index.ts
@@ -1,24 +1,24 @@
 import VideoPlayer from "@media/player";
 import "./index.scss";
-import Hls from "hls.js";
+// import Hls from "hls.js";
 
 // console.log(a);
 
 const player = new VideoPlayer({
   el: ".container",
   videoList: [
-    {
-      label: "高清",
-      url: "https://api.dogecloud.com/player/get.m3u8?vcode=5ac682e6f8231991&userId=17&ext=.m3u8"
-    },
+    // {
+    //   label: "高清",
+    //   url: "https://api.dogecloud.com/player/get.m3u8?vcode=5ac682e6f8231991&userId=17&ext=.m3u8"
+    // },
     {
       label: "高清",
       url: "https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4"
     }
-  ],
-  customType(videoElement, videoObj) {
-    const hls = new Hls();
-    hls.loadSource(videoObj.url);
-    hls.attachMedia(videoElement);
-  }
+  ]
+  // customType(videoElement, videoObj) {
+  //   const hls = new Hls();
+  //   hls.loadSource(videoObj.url);
+  //   hls.attachMedia(videoElement);
+  // }
 });
