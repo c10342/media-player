2686c4df72145e698f448282310a49850d8d48ad
c10342 <1376023740@qq.com>
2021-08-09 19:45:32 +0800

    examples(examples): 测试demo

--- examples/index.ts
+++ examples/index.ts
@@ -8,16 +8,44 @@ const player = new VideoPlayer({
   el: ".container",
   videoList: [
     // {
-    //   label: "高清",
-    //   url: "https://api.dogecloud.com/player/get.m3u8?vcode=5ac682e6f8231991&userId=17&ext=.m3u8"
+    //   label: "标清",
+    //   url: "https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4"
     // },
+    // {
+    //   label: "高清",
+    //   url: "https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4",
+    //   // default:true
+    // }
+    {
+      label: "标清",
+      url: "http://192.168.35.204/upload/video/2082/1_2082.mp4"
+    },
     {
       label: "高清",
-      url: "https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4"
+      url: "http://192.168.35.204/upload/video/2082/2_2082.mp4"
+    },
+    {
+      label: "超清",
+      url: "http://192.168.35.204/upload/video/2082/3_2082.mp4"
     }
   ]
+  // speedList:[
+  //   {
+  //     label:'0.5x',
+  //     value:0.5
+  //   },
+  //   {
+  //     label:'1x',
+  //     value:1
+  //   },
+  //   {
+  //     label:'1.5x',
+  //     value:1.5,
+  //     default:true
+  //   }
+  // ],
   // autoplay: true,
-  // mute:true
+  // muted:true
   // customType(videoElement, videoObj) {
   //   const hls = new Hls();
   //   hls.loadSource(videoObj.url);
