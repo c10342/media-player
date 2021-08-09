1f92e65dc24df299a80abd139adc37cd6bf11e52
c10342 <1376023740@qq.com>
2021-08-09 22:44:03 +0800

    examples(examples): 测试demo

--- examples/index.ts
+++ examples/index.ts
@@ -7,43 +7,43 @@ import "./index.scss";
 const player = new VideoPlayer({
   el: ".container",
   videoList: [
-    // {
-    //   label: "标清",
-    //   url: "https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4"
-    // },
-    // {
-    //   label: "高清",
-    //   url: "https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4",
-    //   // default:true
-    // }
     {
       label: "标清",
-      url: "http://192.168.35.204/upload/video/2082/1_2082.mp4"
+      url: "https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4"
     },
     {
       label: "高清",
-      url: "http://192.168.35.204/upload/video/2082/2_2082.mp4"
+      url: "https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4"
+      // default:true
+    }
+    // {
+    //   label: "标清",
+    //   url: "http://192.168.35.204/upload/video/2082/1_2082.mp4"
+    // },
+    // {
+    //   label: "高清",
+    //   url: "http://192.168.35.204/upload/video/2082/2_2082.mp4"
+    // },
+    // {
+    //   label: "超清",
+    //   url: "http://192.168.35.204/upload/video/2082/3_2082.mp4"
+    // }
+  ],
+  speedList: [
+    {
+      label: "0.5x",
+      value: 0.5
     },
     {
-      label: "超清",
-      url: "http://192.168.35.204/upload/video/2082/3_2082.mp4"
+      label: "1x",
+      value: 1,
+      default: true
+    },
+    {
+      label: "1.5x",
+      value: 1.5
     }
   ]
-  // speedList:[
-  //   {
-  //     label:'0.5x',
-  //     value:0.5
-  //   },
-  //   {
-  //     label:'1x',
-  //     value:1
-  //   },
-  //   {
-  //     label:'1.5x',
-  //     value:1.5,
-  //     default:true
-  //   }
-  // ],
   // autoplay: true,
   // muted:true
   // customType(videoElement, videoObj) {
@@ -51,6 +51,13 @@ const player = new VideoPlayer({
   //   hls.loadSource(videoObj.url);
   //   hls.attachMedia(videoElement);
   // }
+});
+
+player.$on("play", () => {
+  console.log("play");
+});
+player.$on("ratechange", () => {
+  console.log("ratechange");
 });
 
 // const destroyButton = document.querySelector(".destroy");
