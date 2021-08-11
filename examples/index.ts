c8e2dcd3786a886a3c646d3f4fbea50ab128fb2e
c10342 <1376023740@qq.com>
2021-08-11 20:32:22 +0800

    examples(examples): 测试demo

--- examples/index.ts
+++ examples/index.ts
@@ -1,7 +1,7 @@
 import VideoPlayer from "@media/player";
 import "./index.scss";
 
-VideoPlayer.setLang("en");
+// VideoPlayer.setLang("en");
 // import Hls from "hls.js";
 
 // console.log(a);
@@ -46,6 +46,7 @@ const player = new VideoPlayer({
       value: 1.5
     }
   ]
+  // hotkey:true
   // live:true
   // autoplay: true,
   // muted:true
@@ -63,8 +64,51 @@ player.$on("ratechange", () => {
   console.log("ratechange");
 });
 
-// const destroyButton = document.querySelector(".destroy");
+document.querySelector(".play")?.addEventListener("click", function () {
+  player.play();
+});
+document.querySelector(".pause")?.addEventListener("click", function () {
+  player.pause();
+});
+document.querySelector(".seek")?.addEventListener("click", function () {
+  player.seek(100);
+});
+document.querySelector(".setNotice")?.addEventListener("click", function () {
+  player.setNotice("你好", 1000);
+});
+document
+  .querySelector(".switchQuality")
+  ?.addEventListener("click", function () {
+    player.switchDefinition(1);
+  });
+document.querySelector(".setSpeed")?.addEventListener("click", function () {
+  player.setSpeed(1.7);
+});
+document.querySelector(".setVolume")?.addEventListener("click", function () {
+  player.setVolume(0.8);
+});
+document.querySelector(".toggle")?.addEventListener("click", function () {
+  player.toggle();
+});
+document.querySelector(".fullscreen")?.addEventListener("click", function () {
+  player.fullScreen.request("web");
+});
+document.querySelector(".videoElement")?.addEventListener("click", function () {
+  console.log(player.videoElement);
+});
+document.querySelector(".currentTime")?.addEventListener("click", function () {
+  console.log(player.currentTime);
+});
+document.querySelector(".duration")?.addEventListener("click", function () {
+  console.log(player.duration);
+});
+document.querySelector(".volume")?.addEventListener("click", function () {
+  console.log(player.volume);
+});
+document.querySelector(".paused")?.addEventListener("click", function () {
+  console.log(player.paused);
+});
 
-// destroyButton?.addEventListener("click", function () {
-//   player.destroy();
-// });
+document.querySelector(".destroy")?.addEventListener("click", function () {
+  player.destroy();
+});
