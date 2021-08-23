a215e3842a824506cb00d797d2497efb3067f46d
c10342 <1376023740@qq.com>
2021-08-23 21:52:17 +0800

    feat(script): 静态文件上传七牛云

---
+++ scripts/config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const userInfo = require("./userInfo");
+module.exports = {
+  // 七牛云配置信息
+  accessKey: userInfo.accessKey,
+  secretKey: userInfo.secretKey,
+  bucket: "lin-player",
+  // 上传到七牛云的目录
+  basePath: path.join(__dirname, "../docs/.vuepress/dist")
+};
