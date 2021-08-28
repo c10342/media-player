3681fa72a2312f43dec20af4b25e9904d4ad3132
c10342 <1376023740@qq.com>
2021-08-28 15:19:37 +0800

    docs(config.js): 删除没用代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,12 +24,7 @@ module.exports = {
     }
   },
   head: [
-    // ['link', { rel: 'icon', href: `/logo.png` }],
-    // ['script', { src: 'https://cdn.jsdelivr.net/npm/flv.js/dist/flv.min.js' }],
-    // ['script', { src: 'https://cdn.jsdelivr.net/npm/hls.js/dist/hls.min.js' }],
-    // ['script', { src: 'https://cdn.jsdelivr.net/npm/dashjs/dist/dash.all.min.js' }],
-    // ['script', { src: 'https://cdn.jsdelivr.net/webtorrent/latest/webtorrent.min.js' }],
-    // ['script', { src: 'https://cdn.jsdelivr.net/npm/dplayer/dist/DPlayer.min.js' }],
+    // ['link', { rel: 'icon', href: `/logo.png` }]
   ],
   themeConfig: {
     repo: "c10342/media-player",
