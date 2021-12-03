77a98b982080fe6f00e418fefe6ae403f625af9d
c10342 <1376023740@qq.com>
2021-12-03 17:51:51 +0800

    refactor(player/tip): 重构tip组件

---
+++ packages/player/subSrc/components/video-tip.ts
@@ -0,0 +1,65 @@
+import { isUndef, parseHtmlToDom } from "@lin-media/utils";
+import { PlayerEvents } from "../config/event";
+import MediaPlayer from "../index";
+import TipTpl from "../templates/tip.art";
+class VideoTip {
+  // 播放器实例
+  private _playerInstance: MediaPlayer;
+  // 组件根元素
+  private _compRootElement: HTMLElement;
+  // 定时器
+  private timer: number | null;
+  constructor(playerInstance: MediaPlayer, slotElement: HTMLElement) {
+    // 播放器实例
+    this._playerInstance = playerInstance;
+    this._initDom(slotElement);
+    this._initListener();
+  }
+
+  private _initListener() {
+    this._on(PlayerEvents.DESTROY, this._destroy.bind(this));
+  }
+
+  private _initDom(slotElement: HTMLElement) {
+    const html = TipTpl();
+    this._compRootElement = parseHtmlToDom(html);
+    slotElement.appendChild(this._compRootElement);
+  }
+
+  // 设置通知
+  $setNotice(notice: string, time?: number) {
+    this._destroyTimer();
+    this._showTip(notice);
+    // 2秒后隐藏
+    this.timer = window.setTimeout(() => {
+      this._hideTip();
+    }, time || 2000);
+  }
+  // 销毁定时器
+  private _destroyTimer() {
+    if (!isUndef(this.timer)) {
+      clearTimeout(this.timer);
+      this.timer = null;
+    }
+  }
+  // 显示提示
+  private _showTip(tip: string) {
+    this._compRootElement.innerHTML = tip;
+    this._compRootElement.style.opacity = "1";
+  }
+  // 隐藏提示
+  private _hideTip() {
+    this._compRootElement.style.opacity = "";
+  }
+
+  // 事件监听
+  private _on(eventName: string, handler: Function) {
+    this._playerInstance.$eventBus.$on(eventName, handler);
+  }
+
+  private _destroy() {
+    this._destroyTimer();
+  }
+}
+
+export default VideoTip;
