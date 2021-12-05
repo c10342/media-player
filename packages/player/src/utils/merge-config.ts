a82d5f6e261068430135643d56ff9dfc5fa5a0d6
c10342 <1376023740@qq.com>
2021-12-05 15:45:38 +0800

    fix(player/merge-config): 配置合并完成后，插件去重

--- packages/player/src/utils/merge-config.ts
+++ packages/player/src/utils/merge-config.ts
@@ -1,4 +1,5 @@
-import { deepClone, isPlainObject } from "@lin-media/utils";
+import { deepClone, isArray, isPlainObject } from "@lin-media/utils";
+import getPluginName from "./get-plugin-name";
 
 /**
  *
@@ -10,6 +11,7 @@ export default function mergeConfig(targetObj: any, fromObj: any) {
   const hasKey = (key: string, data: any) => {
     return key in data;
   };
+  // 先处理目标对象上面的
   for (const key in targetObj) {
     if (key === "controls") {
       if (isPlainObject(targetObj[key])) {
@@ -27,7 +29,7 @@ export default function mergeConfig(targetObj: any, fromObj: any) {
       config[key] = targetObj[key];
     }
   }
-
+  // 在处理默认配置
   for (const key in fromObj) {
     if (key === "controls") {
       if (isPlainObject(config[key])) {
@@ -66,6 +68,22 @@ export default function mergeConfig(targetObj: any, fromObj: any) {
         config[key] = fromObj[key];
       }
     }
+  }
+  // 插件去重
+  if (isArray(config.plugins) && config.plugins.length > 0) {
+    const plugins: Function[] = config.plugins.slice();
+    const pluginsArr: Function[] = [];
+    const installed = (item: Function) => {
+      return pluginsArr.some((ctor: any) => {
+        return getPluginName(ctor) === getPluginName(item);
+      });
+    };
+    plugins.forEach((item) => {
+      if (!installed(item)) {
+        pluginsArr.push(item);
+      }
+    });
+    config.plugins = pluginsArr;
   }
 
   return config;
