f24e10ab53255a31533ab8ffad1475915167f450
c10342 <1376023740@qq.com>
2021-12-31 14:05:58 +0800

    feat(player): 修改合并配置的方法

--- packages/player/src/utils/merge-config.ts
+++ packages/player/src/utils/merge-config.ts
@@ -1,75 +1,21 @@
 import { deepClone, isArray, isPlainObject } from "@lin-media/utils";
+import { PlayerOptions } from "../types";
 import getPluginName from "./get-plugin-name";
 
-/**
- *
- * @param targetObj 目标值来源
- * @param fromObj 默认值来源
- */
-export default function mergeConfig(targetObj: any, fromObj: any) {
-  const config: any = {};
-  const hasKey = (key: string, data: any) => {
-    return key in data;
-  };
-  // 先处理目标对象上面的
-  for (const key in targetObj) {
-    if (key === "controls") {
-      if (isPlainObject(targetObj[key])) {
-        config[key] = {
-          ...targetObj[key]
-        };
-      } else if (targetObj[key] === false) {
-        config[key] = false;
-      }
-    } else if (key === "plugins") {
-      config[key] = [...targetObj[key]];
-    } else if (key === "customLanguage") {
-      config[key] = deepClone(targetObj[key]);
-    } else {
-      config[key] = targetObj[key];
-    }
-  }
-  // 在处理默认配置
-  for (const key in fromObj) {
-    if (key === "controls") {
-      if (isPlainObject(config[key])) {
-        if (hasKey(key, config)) {
-          config[key] = {
-            ...fromObj[key],
-            ...config[key]
-          };
-        } else {
-          config[key] = {
-            ...fromObj[key]
-          };
-        }
-      } else if (config[key] !== false) {
-        config[key] = {
-          ...fromObj[key]
-        };
-      }
-    } else if (key === "plugins") {
-      if (hasKey(key, config)) {
-        config[key] = [...fromObj[key], ...config[key]];
-      } else {
-        config[key] = [...fromObj[key]];
-      }
-    } else if (key === "customLanguage") {
-      if (hasKey(key, config)) {
-        config[key] = {
-          ...deepClone(fromObj[key]),
-          ...config[key]
-        };
-      } else {
-        config[key] = deepClone(fromObj[key]);
-      }
-    } else {
-      if (!hasKey(key, config)) {
-        config[key] = fromObj[key];
-      }
-    }
-  }
-  // 插件去重
+function hasKey(key: string, data: any) {
+  return key in data;
+}
+
+function getType(data: any) {
+  return Object.prototype.toString.call(data);
+}
+
+function isNeedMerge(data: any) {
+  return isPlainObject(data) || isArray(data);
+}
+
+// 插件去重
+function uniquePlugins(config: PlayerOptions) {
   if (isArray(config.plugins) && config.plugins.length > 0) {
     const plugins: Function[] = config.plugins.slice();
     const pluginsArr: Function[] = [];
@@ -85,6 +31,44 @@ export default function mergeConfig(targetObj: any, fromObj: any) {
     });
     config.plugins = pluginsArr;
   }
+  return config;
+}
 
+/**
+ *
+ * @param targetObj 目标值来源
+ * @param fromObj 默认值来源
+ */
+export default function mergeConfig(targetObj: any, fromObj: any) {
+  // 先进行深拷贝，防止对原数据进行污染
+  targetObj = deepClone(targetObj);
+  fromObj = deepClone(fromObj);
+
+  function merge(target: any, from: any) {
+    if (isArray(target) && isArray(from)) {
+      // 数组直接合并起来
+      return from.concat(target);
+    }
+    if (isPlainObject(target) && isPlainObject(from)) {
+      for (const key in from) {
+        // 目标值和默认值存在相同的key
+        if (hasKey(key, target)) {
+          // 如果类型不同的话，以目标值的为主
+          if (
+            getType(target[key]) === getType(from[key]) &&
+            isNeedMerge(from[key])
+          ) {
+            target[key] = merge(target[key], from[key]);
+          }
+        } else {
+          target[key] = from[key];
+        }
+      }
+    }
+    return target;
+  }
+  const config = merge(targetObj, fromObj);
+  // 插件去重
+  uniquePlugins(config);
   return config;
 }
