7a50c7d4ff6b50bd6612fc41d49820bb429c3202
c10342 <1376023740@qq.com>
2022-01-10 21:52:59 +0800

    fix(utils): 使用cloneNode克隆一份节点出来，否则video标签开启画中画的时候浏览器会崩溃掉

--- packages/utils/src/parse-html-to-dom.ts
+++ packages/utils/src/parse-html-to-dom.ts
@@ -1,4 +1,6 @@
 export default function parseHtmlToDom(html: string) {
-  return new DOMParser().parseFromString(html, "text/html").body
-    .firstChild as HTMLElement;
+  // fix:必须使用cloneNode克隆一份节点出来，否则video标签开启画中画的时候浏览器会崩溃掉
+  return new DOMParser()
+    .parseFromString(html, "text/html")
+    .body.firstChild?.cloneNode(true) as HTMLElement;
 }
