3135e679f885ab5fcd482054de2b13ae00476f49
c10342 <1376023740@qq.com>
2022-04-15 21:54:42 +0800

    feat(shortcut-key.ts): 快捷键改为插件的形式

--- packages/player/src/plugins/shortcut-key.ts
+++ packages/player/src/plugins/shortcut-key.ts
@@ -1,120 +1,114 @@
 import { EventManager } from "@lin-media/utils";
-import { SHORTCUTKEY } from "../config/constant";
 import { CanFocusTagEnum, KeyCodeEnum } from "../config/enum";
 import { PlayerEvents } from "../config/event";
-import MediaPlayer from "../index";
+import { registerPlugin } from "../global-api/plugin";
+import Player from "../player";
+import { PluginApi } from "../types";
 
-class ShortcutKey {
-  static pluginName = SHORTCUTKEY;
+class ShortcutKey implements PluginApi {
   // 播放器实例
-  private _playerInstance: MediaPlayer;
+  private player: Player;
   // 标志位，标记用户是否点击了播放器，也就是播放器是否处于活跃状态
-  private _isFocus = false;
-  private _eventManager = new EventManager();
-  constructor(playerInstance: MediaPlayer) {
-    this._playerInstance = playerInstance;
+  private isFocus = false;
+  private eventManager = new EventManager();
+  constructor(player: Player) {
+    this.player = player;
     // 初始化事件监听
-    this._initListener();
+    this.initListener();
   }
 
-  private _initListener() {
-    this._eventManager.addEventListener({
+  private initListener() {
+    this.eventManager.addEventListener({
       element: document,
       eventName: "click",
-      handler: this._onDocumentClick.bind(this)
+      handler: this.onDocumentClick.bind(this)
     });
-    this._eventManager.addEventListener({
-      element: this._playerInstance.$rootElement,
+    this.eventManager.addEventListener({
+      element: this.player.rootElement,
       eventName: "click",
       handler: this.onPlayerContainerClick.bind(this)
     });
-    this._eventManager.addEventListener({
+    this.eventManager.addEventListener({
       element: document,
       eventName: "keydown",
-      handler: this._onDocumentKeydown.bind(this)
+      handler: this.onDocumentKeydown.bind(this)
     });
-    this._on(PlayerEvents.DESTROY, this._destroy.bind(this));
   }
 
   // 点击播放器外的元素时吧标志位置为false
-  private _onDocumentClick() {
-    this._isFocus = false;
+  private onDocumentClick() {
+    this.isFocus = false;
   }
 
   // 点击播放器
   private onPlayerContainerClick(event: MouseEvent) {
     // 要组织冒泡，防止冒泡到document中
     event.stopPropagation();
-    this._isFocus = true;
+    this.isFocus = true;
   }
   // 键盘事件处理
-  private _onDocumentKeydown(event: KeyboardEvent) {
+  private onDocumentKeydown(event: KeyboardEvent) {
     // 获取获得焦点的元素
     const activeTag: any = document.activeElement?.nodeName.toUpperCase();
     // 获取元素是否可编辑
     const editable = document.activeElement?.getAttribute("contenteditable");
     // 看看获得焦点的元素是不是input或者textarea
     const flag = Object.values(CanFocusTagEnum).includes(activeTag);
-    if (!flag && editable !== "" && editable !== "true" && this._isFocus) {
+    if (!flag && editable !== "" && editable !== "true" && this.isFocus) {
       // 不可编辑并且播放器处于活跃状态
-      this._handleKey(event);
+      this.handleKey(event);
     }
   }
 
-  private _handleKey(event: KeyboardEvent) {
-    const { live } = this._playerInstance.$options;
-    const playerInstance = this._playerInstance;
+  private handleKey(event: KeyboardEvent) {
+    const { live } = this.player.options;
+    const player = this.player;
     switch (event.keyCode) {
       case KeyCodeEnum.space:
         event.preventDefault();
         // 按下空格键切换播放状态
-        playerInstance.toggle();
-        this._emit(PlayerEvents.KEYBOARD_SPACE);
+        player.toggle();
+        this.player.$emit(PlayerEvents.KEYBOARD_SPACE);
         break;
       case KeyCodeEnum.left:
         event.preventDefault();
         // 按下左箭头，时间后退5秒
         if (!live) {
           // 直播不能后退
-          playerInstance.seek(playerInstance.currentTime - 5);
+          player.seek(player.currentTime - 5);
         }
-        this._emit(PlayerEvents.KEYBOARD_LEFT);
+        this.player.$emit(PlayerEvents.KEYBOARD_LEFT);
         break;
       case KeyCodeEnum.right:
         event.preventDefault();
         if (!live) {
           // 按下右箭头，时间前进5秒
-          playerInstance.seek(playerInstance.currentTime + 5);
+          player.seek(player.currentTime + 5);
         }
-        this._emit(PlayerEvents.KEYBOARD_RIGHT);
+        this.player.$emit(PlayerEvents.KEYBOARD_RIGHT);
         break;
       case KeyCodeEnum.up:
         event.preventDefault();
         // 按下上箭头，音量增大
-        playerInstance.setVolume(playerInstance.volume + 0.1);
-        this._emit(PlayerEvents.KEYBOARD_UP);
+        player.setVolume(player.volume + 0.1);
+        this.player.$emit(PlayerEvents.KEYBOARD_UP);
         break;
       case KeyCodeEnum.down:
         event.preventDefault();
         // 按下下箭头，音量减少
-        playerInstance.setVolume(playerInstance.volume - 0.1);
-        this._emit(PlayerEvents.KEYBOARD_DOWN);
+        player.setVolume(player.volume - 0.1);
+        this.player.$emit(PlayerEvents.KEYBOARD_DOWN);
         break;
     }
   }
 
-  // 事件监听
-  private _on(eventName: string, handler: Function) {
-    this._playerInstance.$eventBus.$on(eventName, handler);
-  }
-
-  private _emit(eventName: string, data?: any) {
-    this._playerInstance.$eventBus.$emit(eventName, data);
-  }
-
-  private _destroy() {
-    this._eventManager.removeEventListener();
+  destroy() {
+    this.eventManager.removeEventListener();
   }
 }
+
+registerPlugin("ShortcutKey", ShortcutKey, {
+  init: true
+});
 
 export default ShortcutKey;
