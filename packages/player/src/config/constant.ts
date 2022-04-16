ab3e7bf09e4cfd04b0786d027b65ccad05393b4e
c10342 <1376023740@qq.com>
2022-04-16 14:53:03 +0800

    style(constant.ts): 删除无用常量

--- packages/player/src/config/constant.ts
+++ packages/player/src/config/constant.ts
@@ -10,35 +10,3 @@ export const BODYHIDDENCLASSNAME = "player-overflow-hidden";
 export const FLOATBUTTONENTERCLASSNAME = "player-float-button-enter";
 
 export const FLOATBUTTONLEAVECLASSNAME = "player-float-button-leave";
-
-export const VIDEOFLOATBUTTON = "VideoFloatButton";
-
-export const DOMRESIZEOBSERVER = "DomResizeObserver";
-
-export const SHORTCUTKEY = "ShortcutKey";
-
-export const VIDEOCONTROLS = "VideoControls";
-
-export const VIDEODEFINITION = "VideoDefinition";
-
-export const VIDEOFULLSCREEN = "VideoFullscreen";
-
-export const VIDEOLIVE = "VideoLive";
-
-export const VIDEOLOADING = "VideoLoading";
-
-export const VIDEOMASK = "VideoMask";
-
-export const VIDEOPLAYBUTTON = "VideoPlayButton";
-
-export const VIDEOPLAYER = "VideoPlayer";
-
-export const VIDEOPROGRESS = "VideoProgress";
-
-export const VIDEOSPEED = "VideoSpeed";
-
-export const VIDEOTIME = "VideoTime";
-
-export const VIDEOTIP = "VideoTip";
-
-export const VIDEOVOLUME = "VideoVolume";
