d6b92bf3ff698b7292e57bce87b94ac30353dc3c
c10342 <1376023740@qq.com>
2022-04-16 14:21:47 +0800

    feat(register-component.ts): 祖册组件

---
+++ packages/player/src/register-component.ts
@@ -0,0 +1,6 @@
+import "./components/video-player";
+import "./components/video-mask";
+import "./components/video-float-button";
+import "./components/video-controls";
+import "./components/video-loading";
+import "./components/video-tip";
