9c66d42d2e9179cdacab97d4d2d90319f9911376
c10342 <1376023740@qq.com>
2022-04-17 14:32:06 +0800

    feat(html5): 提取公共方法

--- packages/player/src/techs/html5.ts
+++ packages/player/src/techs/html5.ts
@@ -1,19 +1,15 @@
-import { EventEmit } from "@lin-media/utils";
+import { EventEmit, canPlayType } from "@lin-media/utils";
 
 import Player from "../player";
 import { SourceItem } from "../types/player";
-import { canVideoPlayType } from "../utils/helper";
 
 const canHandleSourceType = "video/mp4";
 
-const canPlayType = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
+const playType = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
 
 class Html5 extends EventEmit {
   static canHandleSource(sourceItem: SourceItem) {
-    if (
-      sourceItem.type === canHandleSourceType &&
-      canVideoPlayType(canPlayType)
-    ) {
+    if (sourceItem.type === canHandleSourceType && canPlayType(playType)) {
       return "maybe";
     }
     return "";
