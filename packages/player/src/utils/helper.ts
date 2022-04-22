202de11c2f9182052a0ca871b7a2e887bd54a218
c10342 <1376023740@qq.com>
2022-04-22 23:03:49 +0800

    feat(helper.ts): 广播对应事件

--- packages/player/src/utils/helper.ts
+++ packages/player/src/utils/helper.ts
@@ -11,21 +11,21 @@ export function initComponents(
   components: { [key: string]: Component } = {}
 ) {
   const opts = player.options.components ?? {};
-  forEachComponent(name, (name, component, options) => {
+  forEachComponent(name, (name, Component, options) => {
     const init = (config: Record<string, any> = {}) => {
       const defaults = options.defaults;
-      player.$emit(`beforeComponentSetup:${name}`);
-      const instance = new component(player, rootElement, {
+      player.$emit("beforeComponentSetup", { name });
+      const instance = new Component(player, rootElement, {
         ...defaults,
         ...config
       });
       components[name] = instance;
-      player.$emit(`afterComponentSetup:${name}`, instance);
+      player.$emit("afterComponentSetup", { name, component: instance });
     };
     // 先判断构造函数中是否有shouldInit静态函数，判断是否需要进行初始化
     if (
-      isFunction(component.shouldInit) &&
-      !component.shouldInit(player.options)
+      isFunction(Component.shouldInit) &&
+      !Component.shouldInit(player.options)
     ) {
       return;
     }
@@ -48,25 +48,25 @@ export function destroyComponents(
 ) {
   Object.keys(components).forEach((name) => {
     const component = components[name];
-    player.$emit(`beforeComponentDestroy:${name}`, component);
+    player.$emit("beforeComponentDestroy", { name, component });
     if (isFunction(component.destroy)) {
       component.destroy();
     }
-    player.$emit(`afterComponentDestroy:${name}`);
+    player.$emit("afterComponentDestroy", { name });
   });
 }
 
 export function initPlugins(player: Player) {
   const plugins = player.options.plugins || {};
-  forEachPlugins((name, plugin, options) => {
+  forEachPlugins((name, Plugin, options) => {
     const init = (config: Record<string, any> = {}) => {
       const defaults = options.defaults;
-      player.$emit(`beforePluginSetup:${name}`);
-      const instance = new plugin(player, { ...defaults, ...config });
-      player.plugins[name] = instance;
-      player.$emit(`afterPluginSetup:${name}`, instance);
+      player.$emit(`beforePluginSetup`, { name });
+      const plugin = new Plugin(player, { ...defaults, ...config });
+      player.plugins[name] = plugin;
+      player.$emit(`afterPluginSetup`, { name, plugin });
     };
-    if (isFunction(plugin.shouldInit) && !plugin.shouldInit(player.options)) {
+    if (isFunction(Plugin.shouldInit) && !Plugin.shouldInit(player.options)) {
       return;
     }
 
@@ -81,6 +81,17 @@ export function initPlugins(player: Player) {
         init(pluginOptions === true ? {} : pluginOptions);
       }
     }
+  });
+}
+
+export function destroyPlugins(player: Player) {
+  Object.keys(player.plugins).forEach((name) => {
+    const plugin = player.plugins[name];
+    player.$emit(`beforePluginDestroy`, { name, plugin });
+    if (isFunction(plugin.destroy)) {
+      plugin.destroy();
+    }
+    player.$emit(`afterPluginDestroy`, { name });
   });
 }
 
