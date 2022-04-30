07d53ab5b72a70e6a7b325763cb0cf7c6d94fc94
c10342 <1376023740@qq.com>
2022-04-30 20:51:38 +0800

    style(index.ts): 不导出 export

--- packages/player/index.ts
+++ packages/player/index.ts
@@ -4,10 +4,3 @@ import "./src/register-plugin";
 import "./src/register-tech";
 
 export default MediaPlayer;
-
-export * from "./src/types/component";
-export * from "./src/types/plugin";
-export * from "./src/types/hook";
-export * from "./src/types/index";
-export * from "./src/types/player";
-export * from "./src/types/tech";
