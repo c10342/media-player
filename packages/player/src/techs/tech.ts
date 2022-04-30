bd964b05229cdafb6b96741f0c49d9ab9fc72792
c10342 <1376023740@qq.com>
2022-04-30 20:17:15 +0800

    feat(tech.ts): 添加ready函数

--- packages/player/src/techs/tech.ts
+++ packages/player/src/techs/tech.ts
@@ -3,12 +3,15 @@ import Player from "../player";
 import { PlayerConfig, SourceItem } from "../types/player";
 
 class Tech extends EventEmit {
+  static id = "Tech";
   static canHandleSource(source: SourceItem, options: PlayerConfig) {
     return "";
   }
   player: Player;
   videoElement: HTMLVideoElement;
   source: SourceItem;
+  private isReady = false;
+  private readyCallback: Array<Function> = [];
   constructor(
     player: Player,
     videoElement: HTMLVideoElement,
@@ -18,6 +21,37 @@ class Tech extends EventEmit {
     this.player = player;
     this.videoElement = videoElement;
     this.source = source;
+    this.player.ready(this.onPlayerReady.bind(this));
+  }
+
+  onPlayerReady() {
+    // todo
+  }
+
+  ready(fn: Function) {
+    if (this.isReady) {
+      fn();
+    } else {
+      this.readyCallback.push(fn);
+    }
+  }
+
+  private runReadyCallback() {
+    const list = this.readyCallback.slice();
+    if (list.length === 0) {
+      return;
+    }
+    this.readyCallback = [];
+    list.forEach((fn) => fn());
+  }
+
+  triggerReady() {
+    if (this.isReady) {
+      return;
+    }
+    this.isReady = true;
+    this.$emit("ready");
+    this.runReadyCallback();
   }
 
   destroy() {
