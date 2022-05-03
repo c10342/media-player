e442fe56f73b94cfbd9caa1a1d7bdffa18d7b1af
c10342 <1376023740@qq.com>
2022-05-03 22:30:05 +0800

    feat(register-plugin): shortcutKey插件在非移动端环境下才初始化

--- packages/player/src/register-plugin.ts
+++ packages/player/src/register-plugin.ts
@@ -2,6 +2,7 @@ import ShortcutKey from "./plugins/shortcut-key";
 import DomResizeObserver from "./plugins/resize-observer";
 import { registerPlugin } from "./global-api/plugin";
 import Plugin from "./plugins/plugin";
+import { isMobile } from "@lin-media/utils";
 
 const options = { init: true };
 
@@ -9,4 +10,4 @@ registerPlugin("Plugin", Plugin);
 
 registerPlugin("DomResizeObserver", DomResizeObserver, options);
 
-registerPlugin("ShortcutKey", ShortcutKey, options);
+registerPlugin("ShortcutKey", ShortcutKey, { init: !isMobile() });
